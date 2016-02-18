d02bc666816a03e02d9fc26f65b22d87892c0c3b
Nicholas Thompson <nick@fullfatthings.com>
2016-02-18 00:10:47 +0000

    Removing hard coded index key.

--- sync/import.php
+++ sync/import.php
@@ -1,15 +1,21 @@
 #!/usr/bin/env php
 <?php
+
+if (!array_key_exists('algolia_key', $_ENV)) {
+  echo "No Algolia key. Please export one.\n";
+  exit;
+}
+
 require __DIR__ . '/vendor/autoload.php';
 
-$client = new \AlgoliaSearch\Client('A644RMPSD6', 'e4792e902393bd5fe627ba1ffdfb8e56');
+$client = new \AlgoliaSearch\Client('A644RMPSD6', $_ENV['api_key']);
 $index = $client->initIndex('prod_drupal_modules');
 
 
 $url = 'https://www.drupal.org/api-d7/node.json?type=project_module&status=1&field_project_type=full&limit=20';
-$url = './data.json';
+
 $i = 0;
-$max_pages = 0;
+$max_pages = 10;
 
 do {
   echo "Processing: {$url}... ";
@@ -26,6 +32,13 @@ do {
   $index->addObjects($batch);
 
   $url = (++$i < $max_pages) ? $data->next : FALSE;
+
+  // Hack to fix https://www.drupal.org/node/2670512#comment-10867800
+  if ($url) {
+    if (strpos($url, 'node?') !== FALSE) {
+      $url = str_replace('node?', 'node.json?', $url);
+    }
+  }
 
   echo "Done!\n";
 } while($url);
